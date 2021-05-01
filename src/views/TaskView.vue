0cc99b368eb7ec13088bbc11b3f421c5aea9894a
Pascal Wichmann <wichmann@informatik.uni-hamburg.de>
2021-05-01 09:42:04 +0200

    task JSON import/export

--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -22,6 +22,16 @@
     </p>
 
     <SubTaskList :task="task" />
+
+    <h2>Import/Export</h2>
+    <div class="toast toast-error"
+         v-if="jsonInvalid">
+      Ungültige Eingabe.
+    </div>
+    <textarea class="form-input"
+              v-model="taskJson"
+              :class="{ 'is-error': jsonInvalid }"
+              rows="20"></textarea>
   </div>
   <div v-else>
     <h1>Task Not Found</h1>
@@ -34,6 +44,7 @@ import {taskStore} from "@/store/task";
 import router from "@/router";
 import {Task} from "@/models/task";
 import SubTaskList from "@/components/SubTaskList";
+import {deserialize, serialize} from "serializr";
 
 export default {
   name: 'TaskView',
@@ -52,21 +63,43 @@ export default {
       }
       return task
     })
+    const taskJson = ref('')
+    const jsonInvalid = ref(false)
 
     watchEffect(() => {
       if (task.value === undefined) {
         return
       }
       taskName.value = task.value.name
+      taskJson.value = JSON.stringify(serialize(Task, task.value), null, 2)
+      jsonInvalid.value = false
     })
     watch(taskName, name => {
       const newTask = Object.assign(new Task(), task.value)
       newTask.name = name
       taskStore.updateTask(newTask)
     })
+    watch(taskJson, value => {
+      if (task.value === undefined) {
+        return
+      }
+      let newTask = new Task()
+      try {
+         newTask = deserialize(Task, JSON.parse(value))
+      } catch (e) {
+        console.warn(e)
+        jsonInvalid.value = true
+        return
+      }
+      jsonInvalid.value = false
+      newTask.id = task.value.id
+      taskStore.updateTask(newTask)
+    })
     return {
       task,
       taskName,
+      taskJson,
+      jsonInvalid,
       deleteTask () {
         if (!confirm('Really delete task „' + task.value.name + '“?')) {
           return
