cade3ed6b0dc6ccb49d96fac05c6ff0520281cc7
Pascal Wichmann <wichmann@informatik.uni-hamburg.de>
2021-05-03 16:29:10 +0200

    Warn about local data storage

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="container grid-xl">
+    <div v-if="!storageWarningDismissed"
+         class="toast">
+      <p>
+        All data is only stored within the browser's local storage!
+        Save the exported JSON data to a file to persist or share it.
+        <button class="btn btn-link float-right"
+                @click="storageWarningDismissed = !storageWarningDismissed">
+          <i class="icon icon-cross"></i>
+        </button>
+      </p>
+    </div>
     <router-view />
   </div>
 </template>
 
+<script>
+import {ref} from 'vue'
+
+export default {
+  name: 'App',
+  setup () {
+    return {
+      storageWarningDismissed: ref(false)
+    }
+  }
+}
+</script>
