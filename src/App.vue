6b6d886c43b7d18a88ffca2db2c6befb71db4fcb
Pascal Wichmann <wichmann@informatik.uni-hamburg.de>
2021-07-19 16:14:41 +0200

    copyright …

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="container grid-xl">
+  <div class="main-content container grid-xl">
     <div v-if="workingLocally && !storageWarningDismissed"
          class="toast">
       <p>
         All data is only stored within the browser's local storage!
         Save the exported JSON data to a file to persist or share it.
-        <a @click="undismissAuth" class="c-hand">Login</a>
+        <a class="c-hand" @click="undismissAuth">Login</a>
         <button class="btn btn-link float-right"
                 @click="storageWarningDismissed = !storageWarningDismissed">
           <i class="icon icon-cross"></i>
@@ -15,6 +15,10 @@
     <router-view />
 
     <AuthModal v-if="workingLocally && !authDismissed" />
+  </div>
+  <div class="footer">
+    Korrekturtool by Pascal Wichmann.
+    Copyright &copy; {{ copyrightYear }}
   </div>
 </template>
 
@@ -27,7 +31,9 @@ export default {
   name: 'App',
   components: {AuthModal},
   setup () {
+    const currentYear = (new Date()).getFullYear()
     return {
+      copyrightYear: currentYear > 2021 ? '2021-' + currentYear.toString() : '2021',
       storageWarningDismissed: ref(false),
       workingLocally: computed(() => {
         return authStore.getState().workingLocally
