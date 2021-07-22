e33e4611f6858f64c739d412319ab622d09dcbf1
Pascal Wichmann <wichmann@informatik.uni-hamburg.de>
2021-07-22 10:21:20 +0200

    New Grading: Scroll to top

--- src/views/GradeTaskView.vue
+++ src/views/GradeTaskView.vue
@@ -128,6 +128,7 @@ export default {
       const newGrading = new Grading()
       newGrading.task = task.value
       gradingStore.setCurrentGrading(newGrading)
+      window.scrollTo(0, 0)
     }
 
     const grading = computed(() => gradingStore.getState().currentGrading)
